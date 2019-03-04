b2b1d06a084fc30bf53e4ff9e5145c86339da188
Ajay Qi <ajayq@uw.edu>
2019-03-03 20:14:44 -0800

    Added Map.js file

---
+++ src/Map.js
@@ -0,0 +1,91 @@
+import React, { Component } from 'react';
+
+const TEST_YELP = {
+    "rating": 4,
+    "price": "$$",
+    "hours": [
+        {
+        "hours_type": "REGULAR",
+        "open": [
+            {
+            "is_overnight": false,
+            "end": "2300",
+            "day": 0,
+            "start": "1000"
+            },
+            {
+            "is_overnight": false,
+            "end": "2300",
+            "day": 1,
+            "start": "1000"
+            },
+            {
+            "is_overnight": false,
+            "end": "2300",
+            "day": 2,
+            "start": "1000"
+            },
+            {
+            "is_overnight": false,
+            "end": "2300",
+            "day": 3,
+            "start": "1000"
+            },
+            {
+            "is_overnight": false,
+            "end": "0000",
+            "day": 4,
+            "start": "1000"
+            },
+            {
+            "is_overnight": false,
+            "end": "0000",
+            "day": 5,
+            "start": "1000"
+            },
+            {
+            "is_overnight": false,
+            "end": "2300",
+            "day": 6,
+            "start": "1000"
+            }
+        ],
+        "is_open_now": true
+        }
+    ],
+    "photos": [
+        "http://s3-media4.fl.yelpcdn.com/bphoto/howYvOKNPXU9A5KUahEXLA/o.jpg",
+        "http://s3-media3.fl.yelpcdn.com/bphoto/I-CX8nioj3_ybAAYmhZcYg/o.jpg",
+        "http://s3-media2.fl.yelpcdn.com/bphoto/uaSNfzJUiFDzMeSCwTcs-A/o.jpg"
+    ],
+    "id": "north-india-restaurant-san-francisco",
+    "categories": [
+        {
+        "alias": "indpak",
+        "title": "Indian"
+        }
+    ],
+    "review_count": 551,
+    "name": "North India Restaurant",
+    "url": "https://www.yelp.com/biz/north-india-restaurant-san-francisco",
+    "coordinates": {
+        "latitude": 37.787789124691,
+        "longitude": -122.399305736113
+    },
+    "image_url": "https://s3-media1.fl.yelpcdn.com/bphoto/howYvOKNPXU9A5KUahEXLA/o.jpg",
+    "location": {
+        "city": "San Francisco",
+        "country": "US",
+        "address2": "",
+        "address3": "",
+        "state": "CA",
+        "address1": "123 Second St",
+        "zip_code": ""
+    }
+}
+
+export class Map extends Component {
+    render() {
+
+    }
+}
