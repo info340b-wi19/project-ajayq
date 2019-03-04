b87abc9cfa13562667227a11a0342cf16ac40f95
MatthewSVogt <Matthewvote@gmail.com>
2019-03-04 15:23:31 -0800

    fetch module

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import 'https://unpkg.com/leaflet@1.4.0/dist/leaflet.css';
+//import 'https://unpkg.com/leaflet@1.4.0/dist/leaflet.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import Map from './Map'
 
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<Map />, document.getElementById('root'));
+//ReactDOM.render(<Map />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
