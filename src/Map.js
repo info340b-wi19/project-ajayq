980add80a9a449d7ccccfce3e2a23f20a87c1b61
Ajay Qi <ajayq@uw.edu>
2019-03-03 21:23:08 -0800

    Attempted to add leaflet

--- src/Map.js
+++ src/Map.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
 
 const TEST_YELP = {
     "rating": 4,
@@ -84,8 +85,16 @@ const TEST_YELP = {
     }
 }
 
-export class Map extends Component {
+export default class l extends Component {
     render() {
-
+        return (<Map center={[51.505, -0.09]} zoom={13}>
+            <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+            />
+            <Marker position={[51.505, -0.09]}>
+            <Popup>A pretty CSS3 popup.<br />Easily customizable.</Popup>
+            </Marker>
+        </Map>)
     }
 }
