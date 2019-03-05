c23df921db32d28616c70a3e23960ea17236c274
MatthewSVogt <Matthewvote@gmail.com>
2019-03-04 17:55:14 -0800

    fetch works

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
-import NavbarPage from './NavbarPage'
+import RestaurantCard from './RestaurantCard';
 
-class App extends Component {
-    render() {
-        return <NavbarPage />
-    }
+const config = {
+  headers: {'Authorization': 'Bearer API key'},
+  params: {
+    term: 'tacos',
+    location: 'main 123st'
+  }
+};
+
+let locationSearched = "seattle";
+let apiKey = "TMcGNjy6GrtE3xc5EFSCuNfX202sXbkyE9yq2pguPydM5ajHiNEjp8nd7qdOhZgHCO8FFk2OtpEolPd-6iTMVsp_frK2N0tx2gc1NEH0EaloyEWx-BJs1bHXT7F9XHYx";
+
+export default class App extends Component {
+  
+  render(){
+    return (
+        <RestaurantCard />      
+    );
+  }
 }
-
-export default App;
