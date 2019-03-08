c941755c8389726f81be441e5110f5f3df3529d6
Student User <studentuser@D-10-18-241-171.dhcp4.washington.edu>
2019-03-08 15:39:59 -0800

    Attempted filters

--- src/App.js
+++ src/App.js
@@ -15,7 +15,11 @@ export default class App extends Component {
             businesses : [],
             lat: 37.787789124691,
             long:-122.399305736113,
-            addAlert:false
+            addAlert:false,
+            category: "",
+            price: "1",
+            distance: 10000,
+            limit:20,
         }; 
         if (navigator.geolocation !== undefined) {
             navigator.geolocation.getCurrentPosition((position) => {
@@ -36,11 +40,21 @@ export default class App extends Component {
         })
     }
 
+    handleSelect = (evt) => {
+        evt.preventDefault();
+        let selectName = evt.target.name;
+        let selectedValue = evt.target.value;
+        this.setState({ [selectName]: selectedValue }, () => {
+            this.generalYelpCall(this.state.navValue)
+        })
+      }
+
     // Preforms a yelp api fetch, look at the other yelp call for our reasoning on the different fetch
     // sets the state of the businesses within the location.
     // uses the current state's latitude and longitude.
     yelpCall = () => {
-        axios.get(`${'https://cors-anywhere.herokuapp.com/'}https://api.yelp.com/v3/businesses/search?latitude=${this.state.lat}&longitude=${this.state.long}`, {
+        axios.get(`${'https://cors-anywhere.herokuapp.com/'}https://api.yelp.com/v3/businesses/search?latitude=${this.state.lat}
+        &longitude=${this.state.long}&categories=${this.state.category}&price=${this.state.price}&radius=${this.state.distance}&limit=${this.state.limit}`, {
             headers: {
                 Authorization: `Bearer ${apiKey}`
             }
@@ -61,7 +75,8 @@ export default class App extends Component {
     // this function updates the state and takes in a location argument which was originally from the navbar
     // updates the latitude and longitude based on the yelp api response
     generalYelpCall = (arg) => {
-        axios.get(`${'https://cors-anywhere.herokuapp.com/'}https://api.yelp.com/v3/businesses/search?location=${arg}`, {
+        axios.get(`${'https://cors-anywhere.herokuapp.com/'}https://api.yelp.com/v3/businesses/search?latitude=${this.state.lat}
+        &longitude=${this.state.long}&categories=${this.state.category}&price=${this.state.price}&radius=${this.state.distance}&limit=${this.state.limit}`, {
             headers: {
                 Authorization: `Bearer ${apiKey}`
             }
@@ -71,6 +86,8 @@ export default class App extends Component {
                 businesses: res.data.businesses,
                 lat: res.data.region.center.latitude,
                 long: res.data.region.center.longitude
+            }, () => {
+                console.log(this.state);
             })
         })
         .catch((err) => {
@@ -90,7 +107,7 @@ export default class App extends Component {
     render() {
         return (
         <div id="container">
-            <NavbarPage func={this.changeState}/>
+            <NavbarPage func={this.changeState} handleSelect={this.handleSelect}/>
             {
                 this.state.addAlert ? 
                     <div onClick={() => {this.dismissAlert()}}>
